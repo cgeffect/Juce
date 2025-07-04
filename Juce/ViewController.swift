4d86a318cece4163d1021f53e439a2b9014a22d1
Jason <JasonBee@163.com>
2025-07-04 18:07:20 +0800

    Initial Commit

---
+++ Juce/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Juce
+//
+//  Created by Jason on 2025/7/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
